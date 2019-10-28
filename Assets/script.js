b8011e80fef4ace91eb259f03ce5c9cb69cc443d
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-10-27 23:41:14 -0400

    added save functionality to buttons including: click handler, grabbing info in text fields, saving info to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,21 @@
-
-    var now = moment().format('dddd MMM Do YYYY, hh:mm a');
+// allow html to load completely before running any script
+$(document).ready(function () {
+    // use moment object to grab & format the current date on user's device
+    var now = moment().format('dddd, MMM Do YYYY');
+        
     $("#today").text(now);
-    
 
+    // set up click event for buttons using .save
+    $(".save").on("click", function () {
 
+        // getting each time block's textarea id
+        var getTime = $(this).attr("data-id");
+        
+        // grab info in the textarea
+        var comment = $("#" + getTime + "-events").val();
+        
+        // save info to local storage
+        localStorage.setItem(getTime + "-events-info", comment);
 
+    });
+});
